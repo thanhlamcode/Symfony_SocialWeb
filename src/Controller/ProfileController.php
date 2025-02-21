99f16b294ced4039902962bfb9c029ec904d998e
thanhlamcode <lamdoan1122334455@gmail.com>
2025-02-21 13:51:20 +0000

    profile

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/dashboard/profile', name: 'profile')]
+    public function profile(): Response
+    {
+        return $this->render('/profile.html.twig');
+    }
+}
