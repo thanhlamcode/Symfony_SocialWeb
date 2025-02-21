d623a5052c8bfcf5bb1a85ae6a49eae17e2e86ff
thanhlamcode <lamdoan1122334455@gmail.com>
2025-02-21 14:28:49 +0000

    ui: profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -11,6 +11,25 @@ class ProfileController extends AbstractController
     #[Route('/dashboard/profile', name: 'profile')]
     public function profile(): Response
     {
-        return $this->render('/profile.html.twig');
+        // Dữ liệu user (giả lập)
+        $user = [
+            'avatar' => '/images/avatar1.png',
+            'name' => 'Ayman Shaltoni',
+            'email' => 'Aymanshaltoni@gmail.com',
+            'phone' => '+966 5502938123',
+            'role' => 'Senior Product Designer',
+            'bio' => "Hey, I’m a product designer specialized in user interface designs (Web & Mobile) with 10 years of experience...",
+            'interests' => ['UI Design', 'Framer', 'Startups', 'UX', 'Crypto', 'Mobile Apps', 'Webflow'],
+            'banner' => '/images/banner.png',
+            'social_accounts' => [
+                ['url' => 'https://twitter.com/ShaltOni', 'icon' => 'icons/twitter.svg'],
+                ['url' => 'https://instagram.com/shaltoni', 'icon' => 'icons/instagram.svg'],
+                ['url' => 'https://www.linkedin.com/in/aymanshaltoni/', 'icon' => 'icons/linkedin.svg']
+            ]
+        ];
+
+        return $this->render('profile.html.twig', [
+            'user' => $user
+        ]);
     }
 }
