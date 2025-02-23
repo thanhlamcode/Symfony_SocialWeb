1db2e5eedb4b956b6cadaaf3286c781b21047b42
thanhlamcode <lamdoan1122334455@gmail.com>
2025-02-23 07:08:36 +0000

    delete redundant

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -26,28 +26,8 @@ class ProfileController extends AbstractController
 
         $profile = $this->userService->getCurrentUserProfile();
 
-//        dump($profile); exit;
-
-        // Dữ liệu user (giả lập)
-        $user = [
-            'avatar' => '/images/avatar1.png',
-            'name' => 'Ayman Shaltoni',
-            'email' => 'Aymanshaltoni@gmail.com',
-            'phone' => '+966 5502938123',
-            'role' => 'Senior Product Designer',
-            'bio' => "Hey, I’m a product designer specialized in user interface designs (Web & Mobile) with 10 years of experience...",
-            'interests' => ['UI Design', 'Framer', 'Startups', 'UX', 'Crypto', 'Mobile Apps', 'Webflow'],
-            'banner' => '/images/banner.png',
-            'social_accounts' => [
-                ['url' => 'https://twitter.com/ShaltOni', 'icon' => 'icons/github.png'],
-                ['url' => 'https://instagram.com/shaltoni', 'icon' => 'icons/linkdn.png'],
-                ['url' => 'https://www.linkedin.com/in/aymanshaltoni/', 'icon' => 'icons/instagram.png']
-            ],
-            'slug' => 'aymanshaltoni'
-        ];
 
         return $this->render('profile.html.twig', [
-            'user' => $user,
             'profile' => $profile
         ]);
     }
