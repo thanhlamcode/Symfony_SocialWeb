46b0fa5d4e67d37b728e15ed8add1d6bf8bb43a8
thanhlamcode <lamdoan1122334455@gmail.com>
2025-02-26 09:40:03 +0000

    request friend

--- src/Controller/FriendController.php
+++ src/Controller/FriendController.php
@@ -30,73 +30,11 @@ class FriendController extends AbstractController
 
         $friendRequests = $this->friendService->getFriendRequests();
 
-//       dump($otherPeople); exit();
-
-        $friendRequests = [
-            [
-                'name' => 'Như Như',
-                'avatar' => '/images/friend1.jpg',
-                'mutual_friends' => null,
-                'mutual_avatar' => null,
-            ],
-            [
-                'name' => 'Huỳnh Ngọc Huyền Anh',
-                'avatar' => '/images/friend2.jpg',
-                'mutual_friends' => null,
-                'mutual_avatar' => null,
-            ],
-            [
-                'name' => 'Nguyễn An',
-                'avatar' => '/images/friend3.jpg',
-                'mutual_friends' => null,
-                'mutual_avatar' => null,
-            ],
-            [
-                'name' => 'Mai Phi',
-                'avatar' => '/images/friend4.jpg',
-                'mutual_friends' => '4',
-                'mutual_avatar' => '/images/mutual_friend.jpg',
-            ],
-            [
-                'name' => 'Mai Phi',
-                'avatar' => '/images/friend4.jpg',
-                'mutual_friends' => '4',
-                'mutual_avatar' => '/images/mutual_friend.jpg',
-            ]
-        ];
-
-        $users = [
-            [
-                'name' => 'Như Như',
-                'avatar' => '/images/user1.jpg',
-                'mutual_friends' => null,
-                'mutual_avatar' => null,
-            ],
-            [
-                'name' => 'Huỳnh Ngọc Huyền Anh',
-                'avatar' => '/images/user2.jpg',
-                'mutual_friends' => null,
-                'mutual_avatar' => null,
-            ],
-            [
-                'name' => 'Nguyễn An',
-                'avatar' => '/images/user3.jpg',
-                'mutual_friends' => '3',
-                'mutual_avatar' => '/images/mutual_friend.jpg',
-            ],
-            [
-                'name' => 'Mai Phi',
-                'avatar' => '/images/user4.jpg',
-                'mutual_friends' => '5',
-                'mutual_avatar' => '/images/mutual_friend2.jpg',
-            ]
-        ];
-
         return $this->render('friend.html.twig', [
             'friend_requests' => $friendRequests,
-            'users' => $users,
             'profile' => $profile,
-            'otherPeople' => $otherPeople
+            'otherPeople' => $otherPeople,
+            'friendRequests' => $friendRequests,
         ]);
     }
 
