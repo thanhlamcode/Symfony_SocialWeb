e8f4d0f3cd43aae73ca4095e4080ff88e9caa652
thanhlamcode <lamdoan1122334455@gmail.com>
2025-02-28 05:59:22 +0000

    delete reduncdant

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -33,58 +33,8 @@ class DashboardController extends AbstractController
 
         $friends = $this->messageService->getFriendsWithLastMessage($user);
 
-
-        // Duyệt danh sách bạn bè và bổ sung last_message
-//        foreach ($friends as &$friend) {
-//            $lastMessage = $this->messageService->getLastMessage($user->getId(), $friend['id']);
-//            $friend['last_message'] = $lastMessage ? $lastMessage['content'] : 'Chưa có tin nhắn';
-//            $friend['last_message_time'] = $lastMessage ? $lastMessage['sentAt'] : null;
-//        }
-
-        // Danh sách chat (dữ liệu giả lập)
-        $chatList = [
-            [
-                'id' => 1, // Thêm id
-                'name' => 'Nguyễn Văn A',
-                'avatar' => '/images/avatar1.png',
-                'last_message' => 'Bạn khỏe không?',
-                'time' => '10:30 AM'
-            ],
-            [
-                'id' => 2, // Thêm id
-                'name' => 'Trần Thị B',
-                'avatar' => '/images/avatar2.png',
-                'last_message' => 'Mai gặp nhau nhé!',
-                'time' => '09:15 AM'
-            ],
-            [
-                'id' => 3, // Thêm id
-                'name' => 'Lê Minh C',
-                'avatar' => '/images/avatar3.png',
-                'last_message' => 'Haha, đúng rồi đó!',
-                'time' => 'Hôm qua'
-            ]
-        ];
-
-        // Dữ liệu cuộc trò chuyện hiện tại
-        $currentChat = [
-            'name' => 'Nguyễn Văn A',
-            'avatar' => '/images/avatar1.png'
-        ];
-
-        // Tin nhắn mẫu
-        $messages = [
-            ['sender' => $user, 'text' => 'Chào bạn, lâu rồi không gặp!'],
-            ['sender' => 'Nguyễn Văn A', 'text' => 'Đúng rồi đó, dạo này sao rồi?'],
-            ['sender' => $user, 'text' => 'Mình vẫn ổn, công việc dạo này thế nào?'],
-            ['sender' => 'Nguyễn Văn A', 'text' => 'Khá bận, nhưng vẫn ổn.'],
-        ];
-
         return $this->render('dashboard.html.twig', [
             'user' => $user,
-            'chat_list' => $chatList,
-            'current_chat' => $currentChat,
-            'messages' => $messages,
             'profile' => $profile,
             'friends' => $friends,
         ]);
