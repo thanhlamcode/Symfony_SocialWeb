047833ad51b5238f1485d674a7b43a41c8c356af
thanhlamcode <lamdoan1122334455@gmail.com>
2025-03-14 09:10:53 +0000

    change some UI-2

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -32,63 +32,10 @@ class NewsController extends AbstractController
 
         $profile = $this->userService->getCurrentUserProfile();
 
-        $user = [
-            'avatar' => '/images/user_avatar.jpg'
-        ];
-
         // ✅ Lấy danh sách bài viết từ PostService
         $posts = $this->postService->getRecentPosts();
 
-//        dump($posts); exit();
-
-        $stories = [
-            [
-                'name' => 'Thy Nguyen Nguyen',
-                'avatar' => '/images/story1_avatar.jpg',
-                'image' => '/images/story1.jpg'
-            ],
-            [
-                'name' => 'Hạ Băng',
-                'avatar' => '/images/story2_avatar.jpg',
-                'image' => '/images/story2.jpg'
-            ],
-            [
-                'name' => 'Lê Phương Khánh Như',
-                'avatar' => '/images/story3_avatar.jpg',
-                'image' => '/images/story3.jpg'
-            ],
-            [
-                'name' => 'Gia Vinh',
-                'avatar' => '/images/story4_avatar.jpg',
-                'image' => '/images/story4.jpg'
-            ]
-        ];
-
-        $post = [
-            'page_name' => 'GOAL Vietnam',
-            'page_avatar' => '',
-            'time' => '32 phút trước',
-            'content' => [
-                '❌ Duran, Ronaldo tịt ngòi',
-                '❌ Al-Nassr thua ngược 2-3 trên sân nhà',
-                'Và đây là điểm số của Top 4 Saudi Pro League hiện tại 😬'
-            ],
-            'image' => '',
-            'league_table' => [
-                ['rank' => 1, 'name' => 'AL-ITTIHAD', 'matches' => 20, 'goal_difference' => 31, 'points' => 52],
-                ['rank' => 2, 'name' => 'AL-HILAL', 'matches' => 20, 'goal_difference' => 40, 'points' => 48],
-                ['rank' => 3, 'name' => 'AL-QADSIAH', 'matches' => 21, 'goal_difference' => 19, 'points' => 47],
-                ['rank' => 4, 'name' => 'AL-NASSR', 'matches' => 21, 'goal_difference' => 23, 'points' => 44],
-            ],
-            'likes' => 200,
-            'comments' => 50,
-            'shares' => 10
-        ];
-
         return $this->render('news.html.twig', [
-            'user' => $user,
-            'stories' => $stories,
-            'post' => $post,
             'profile' => $profile,
             'friendList' => $friendList,
             'posts' => $posts,
